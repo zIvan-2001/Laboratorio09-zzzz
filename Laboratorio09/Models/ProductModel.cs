aa3ef834937a09118839e6eecfd9a79e40825024
zIvan-2001 <ivan.antonio@tecsup.edu.pe>
2022-05-22 18:20:58 -0500

    Add project files.

---
+++ Laboratorio09/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio09.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }    
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
